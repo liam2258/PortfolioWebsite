960d563ea30cbf6b1751a45fcafcd423db5c5d8f
Liam <helm_of_protection@hotmail.com>
2023-01-05 18:47:56 -0900

    Updated about section, worked on icon grid

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { useRef, useState, useEffect } from 'react';
 import './App.css';
 import Navigation from './Components/Navigation';
 import FadeUp from './Components/animation';
+import self_pic from './Images/self_pic.jpg';
+import jv from './Images/jv.svg';
+import py from './Images/py.svg';
+import post from './Images/post.svg';
+import reactPic from './Images/react.svg';
 
 function App() {
 
@@ -10,7 +15,7 @@ function App() {
       <Navigation/>
       <main>
         <div></div>
-        <div>
+        <div className='container'>
           <section className='intro'>
             <h1>Hello fellow humans, I'm...</h1>
             <h2>Liam Dyer,</h2>
@@ -24,12 +29,31 @@ function App() {
                </div>
           </section>
           <section className='about' id='about'>
-            <div>
-              <FadeUp>
-                <h1>About</h1>
-                <p>Thank you for stopping by my website!</p>
-              </FadeUp>
-            </div>
+            <FadeUp>
+              <h1>About</h1>
+            </FadeUp>
+              <div className='aboutBox'>
+                <FadeUp>
+                  <div>
+                    <p>Thank you for stopping by my website! I've been fascinated with technology
+                      since childhood and always wanted to be part of creating it. Applying to
+                      UAA was one of the ways I set about conquering this goal and have come a long way
+                      from printing "Hello world!" to console. I'm excited to continue my devlopment as an
+                      enginner and build more amazing things.
+                    </p>
+                    <p>Here are some of the technologies I've used...</p>
+                    <div className='iconBox'>
+                      <img src={jv}></img>
+                      <img src={reactPic}></img>
+                      <img src={py}></img>
+                      <img src={post}></img>
+                    </div>
+                  </div>
+                </FadeUp>
+                <FadeUp>
+                  <img className ='self_pic' src={self_pic} alt='Me!'></img>
+                </FadeUp>
+              </div>
           </section>
         </div>
         <div></div>
